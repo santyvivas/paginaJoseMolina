672153e046db6067c36ada2937b3063d42324576
santyvivas <santivivas1809@gmail.com>
2022-11-25 18:56:48 -0500

    archivos iniciales

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Psicólogo Jose Roberto Molina</title>
+    <link rel="shortcut icon" href="logo.png">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php
+            include "header.html";
+        ?>
+    </div>
+</body>
+</html>
