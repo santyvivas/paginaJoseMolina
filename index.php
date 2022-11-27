6e0effc7ba4284a1e3c3a2d5a1c60eb3b3cdde50
santyvivas <santivivas1809@gmail.com>
2022-11-27 01:54:20 -0500

    se agrega parrafos en Quien soy y se cambia el fondo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body class="p-3 mb-2 bg-light text-dark">
+<body>
     <div>
         <?php
         include "header.html";
--- quienSoy.php
+++ quienSoy.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quien soy</title>
+    <link rel="shortcut icon" href="logo.png">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body class="p-3 mb-2 bg-light text-dark">
+<body>
     <div>
         <?php
         include "header.html";
@@ -18,6 +19,19 @@
     </div>
     <div style="border: solid; display: flex; justify-content: center; align-items: center;">
         <img src="foto_jose.JPG" alt="foto psicólogo Jose Roberto Molina" title="Psicólogo Jose Roberto Molina" style="width: 350px">
+    </div>
+    <div class="about-me">
+        <p>
+            Soy Jose Roberto Molina, Psicologo especialista en psicologia educativa y con énfasis en psicología clínica,
+            tengo 27 años, cuento con 2 años de experiencia en el ámbito clinico,
+            manejo terapias cognitivas conductuales, mindfulness, reiki, y uno de mis fuertes son las atenciones a
+            dificultades que todos tenemos en algun momento de nuestras vidas, ansiedad, depresión, estres, duelos por
+            perdidades de seres queridos, duelos amorosos, problemas de auto imagen, entre otros.
+        </p>
+        <p>
+        Actualmente estoy cursando una maestría en administración y gestion de recursos humanos, en la Universidad 
+        Internacional de la Rioja ( España ). Soy una persona carismática, muy abierta de mente, alegre y con una buena aura.
+        </p>
     </div>
 </body>
 
