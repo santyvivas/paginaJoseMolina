becdad4f5a4bba5efc1347cec490570f4a6430ee
santyvivas <santivivas1809@gmail.com>
2022-11-27 01:26:04 -0500

    se agrega header a todas las paginas y redimension de foto

--- quienSoy.php
+++ quienSoy.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Psicólogo Jose Roberto Molina</title>
-    <link rel="shortcut icon" href="logo.png">
+    <title>Quien soy</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
@@ -16,6 +15,9 @@
         <?php
         include "header.html";
         ?>
+    </div>
+    <div style="border: solid; display: flex; justify-content: center; align-items: center;">
+        <img src="foto_jose.JPG" alt="foto psicólogo Jose Roberto Molina" title="Psicólogo Jose Roberto Molina" style="width: 350px">
     </div>
 </body>
 
